110d7fb419e931f788e0763e0b4b1c6d60c892dc
npes-95 <npe.smith.95@gmail.com>
2023-09-29 19:54:31 +0200

    Update to vcv-rack 2.4.1.

--- Casks/vcv-rack-arm.rb
+++ Casks/vcv-rack-arm.rb
@@ -1,5 +1,5 @@
 cask "vcv-rack-arm" do
-  version "2.4.0"
+  version "2.4.1"
   sha256 "dcc9f0dec531b500bc06b54966f019f39fdd78263e9ef597b118ec9178fa72f9"
 
   url "https://vcvrack.com/downloads/RackFree-#{version}-mac-arm64.pkg"
